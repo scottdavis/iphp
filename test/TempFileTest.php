4813cad0412c51d864f8aa0456c51b84dab4d60d
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2009-12-10 22:20:38 -0500

    cleaned up code formating

--- test/TempFileTest.php
+++ test/TempFileTest.php
@@ -1,52 +1,43 @@
 <?php
-	require_once('PHPUnit/Framework.php');
-	require_once('vfsStream/vfsStream.php');
-	require_once(dirname(__FILE__) . '/mocks/temp_file_mock.php');
-	class TempFileTest extends PHPUnit_Framework_TestCase {
-		
-		public function setUp() {
-			vfsStreamWrapper::register();
-			$root = new vfsStreamDirectory('root');
-			vfsStreamWrapper::setRoot($root);
-		}
-		
-		public function testReturnsFileName() {
-			$this->assertEquals(vfsStream::url('root/test'), TempFileMock::fileName('test'));
-		}
-		
-		public function testWritesToTempFile() {
-			$data = 'WHOA! im Data';
-			$file = TempFileMock::fileName('test');
-			TempFileMock::writeToFile('test', $data);
-			$this->assertEquals($data, file_get_contents($file));
-		}
-		
-		public function testReadFromTempFile() {
-			$data = 'WHOA! im Data';
-			$file = TempFileMock::fileName('test');
-			file_put_contents($file, $data);
-			$this->assertEquals($data, TempFileMock::readFromFile('test'));
-		}
-		
-		public function testReset() {
-			$data = 'WHOA! im Data';
-			TempFileMock::writeToFile('test', $data);
-			$this->assertEquals($data, TempFileMock::readFromFile('test'));
-			TempFileMock::reset('test');
-			$this->assertEquals('', TempFileMock::readFromFile('test'));
-		}
-		
-		public function testClear() {
-			$files = array('test1', 'test2');
-			foreach($files as $file) {
-				TempFileMock::fileName($file);
-			}
-			TempFileMock::clear();
-			foreach($files as $file) {
-				$this->assertFalse(file_exists($file));
-			}
-		}
-		
-		
-		
-	}
+require_once ('PHPUnit/Framework.php');
+require_once ('vfsStream/vfsStream.php');
+require_once (dirname(__FILE__) . '/mocks/temp_file_mock.php');
+class TempFileTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        vfsStreamWrapper::register();
+        $root = new vfsStreamDirectory('root');
+        vfsStreamWrapper::setRoot($root);
+    }
+    public function testReturnsFileName() {
+        $this->assertEquals(vfsStream::url('root/test'), TempFileMock::fileName('test'));
+    }
+    public function testWritesToTempFile() {
+        $data = 'WHOA! im Data';
+        $file = TempFileMock::fileName('test');
+        TempFileMock::writeToFile('test', $data);
+        $this->assertEquals($data, file_get_contents($file));
+    }
+    public function testReadFromTempFile() {
+        $data = 'WHOA! im Data';
+        $file = TempFileMock::fileName('test');
+        file_put_contents($file, $data);
+        $this->assertEquals($data, TempFileMock::readFromFile('test'));
+    }
+    public function testReset() {
+        $data = 'WHOA! im Data';
+        TempFileMock::writeToFile('test', $data);
+        $this->assertEquals($data, TempFileMock::readFromFile('test'));
+        TempFileMock::reset('test');
+        $this->assertEquals('', TempFileMock::readFromFile('test'));
+    }
+    public function testClear() {
+        $files = array('test1', 'test2');
+        foreach($files as $file) {
+            TempFileMock::fileName($file);
+        }
+        TempFileMock::clear();
+        foreach($files as $file) {
+            $this->assertFalse(file_exists($file));
+        }
+    }
+}
--- test/TokenizerTest.php
+++ test/TokenizerTest.php
@@ -1,61 +1,35 @@
 <?php
-	require_once('PHPUnit/Framework.php');
-	require_once(dirname(__FILE__) . '/../lib/tokenizer.php');
- 	class TokenizerTest extends PHPUnit_Framework_TestCase {
-		
-		public function setUp() {
-			$this->klass = new Tokenizer('$foo = "bar";');
-		}
-		
-		public function opData() {
-			return array(
-										array(T_FOREACH, 'foreach'),
-										array(T_IF, 'if'),
-										array(T_ELSE, 'else'),
-										array(T_CLASS, 'class')
-									);
-		}
-		
-		/**
-			* @dataProvider opData
-			*/
-		public function testGetOperatorName($trigger, $name) {
-			$this->assertEquals($this->klass->getOperatorName($trigger), $name);
-		}
-		
-		
-		
-		public function syntaxCheckData() {
-			return array(
-										array('$test = "foo";', true),
-										array('test', true),
-										array('foreach(', false),
-										array('foreach(range(0,10) as $r){ echo $r;}', true),
-										array('class foo }', false),
-										array(substr(file_get_contents(__FILE__), 5, 0), true)
-									);
-		}
-		
-		
-		/**
-		 * @dataProvider syntaxCheckData
-		 */
-		public function testCheckSyntax($code, $pass) {
-			$klass = new Tokenizer($code);
-			$this->assertEquals($pass, $klass->checkSyntax());
-		}
-		
-		
-  	public function testCheckFindForeach() {
-			$klass = new Tokenizer('foreach');
-			$this->assertTrue($klass->isMultiLine());
-		}
-		
-		
-		public function testCheckVariableNotMultiline() {
-			$klass = new Tokenizer('$test = "foo";');
-			$this->assertFalse($klass->isMultiLine());
-		}
-		
-		
-	}
+require_once ('PHPUnit/Framework.php');
+require_once (dirname(__FILE__) . '/../lib/tokenizer.php');
+class TokenizerTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        $this->klass = new Tokenizer('$foo = "bar";');
+    }
+    public function opData() {
+        return array(array(T_FOREACH, 'foreach'), array(T_IF, 'if'), array(T_ELSE, 'else'), array(T_CLASS, 'class'));
+    }
+    /**
+     * @dataProvider opData
+     */
+    public function testGetOperatorName($trigger, $name) {
+        $this->assertEquals($this->klass->getOperatorName($trigger), $name);
+    }
+    public function syntaxCheckData() {
+        return array(array('$test = "foo";', true), array('test', true), array('foreach(', false), array('foreach(range(0,10) as $r){ echo $r;}', true), array('class foo }', false), array(substr(file_get_contents(__FILE__), 5, 0), true));
+    }
+    /**
+     * @dataProvider syntaxCheckData
+     */
+    public function testCheckSyntax($code, $pass) {
+        $klass = new Tokenizer($code);
+        $this->assertEquals($pass, $klass->checkSyntax());
+    }
+    public function testCheckFindForeach() {
+        $klass = new Tokenizer('foreach');
+        $this->assertTrue($klass->isMultiLine());
+    }
+    public function testCheckVariableNotMultiline() {
+        $klass = new Tokenizer('$test = "foo";');
+        $this->assertFalse($klass->isMultiLine());
+    }
+}

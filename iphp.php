574acfeb0dd961e457ba06bfa629e4cea25f9f2f
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2009-12-10 22:05:40 -0500

    php_beautifier

--- iphp.php
+++ iphp.php
@@ -1,6 +1,5 @@
 <?php
-
-	require_once(dirname(__FILE__) . '/lib/temp_file.php');
+require_once (dirname(__FILE__) . '/lib/temp_file.php');
 // vim: set expandtab tabstop=4 shiftwidth=4:
 
 /**
@@ -12,25 +11,19 @@
  *
  * Use ctl-d to exit the shell, or enter the command "exit".
  */
-class iphp
-{
+class iphp {
     protected $lastResult = NULL;
     protected $lastCommand = NULL;
     protected $prompt = '> ';
     protected $autocompleteList = array();
     protected $options = array();
-		protected $specialCommands = array('exit', 'reload!', 'help');
+    protected $specialCommands = array('exit', 'reload!', 'help');
     protected $phpExecutable = null;
-		protected $running = true;
-
-    const OPT_TAGS_FILE     = 'tags';
-    const OPT_REQUIRE       = 'require';
-    
-
-    const OPT_TMP_DIR       = 'tmp_dir';
+    protected $running = true;
+    const OPT_TAGS_FILE = 'tags';
+    const OPT_REQUIRE = 'require';
+    const OPT_TMP_DIR = 'tmp_dir';
     const OPT_PROMPT_HEADER = 'prompt_header';
-
-
     /**
      * Constructor
      *
@@ -40,7 +33,6 @@ class iphp
     public function __construct($options = array()) {
         $this->initialize($options);
     }
-
     private function initialize($options = array()) {
         $this->initializeOptions($options);
         $this->initializeTempFiles();
@@ -49,28 +41,17 @@ class iphp
         $this->phpExecutable = self::PHPExecutableLocation();
         $this->requireFiles();
     }
-
     private function initializeOptions($options = array()) {
         // merge opts
         $this->options = array_merge(array(
-                                            // default options
-                                            self::OPT_TAGS_FILE     => NULL,
-                                            self::OPT_REQUIRE       => NULL,
-                                            self::OPT_TMP_DIR       => NULL,
-                                            self::OPT_PROMPT_HEADER => $this->getPromptHeader(),
-                                            self::OPT_PHP_BIN       => $this->getPhpBin(),
-                                          ), $options);
+        // default options
+        self::OPT_TAGS_FILE => NULL, self::OPT_REQUIRE => NULL, self::OPT_TMP_DIR => NULL, self::OPT_PROMPT_HEADER => $this->getPromptHeader(), self::OPT_PHP_BIN => $this->getPhpBin(),), $options);
     }
-
-
-
     private function initializeTempFiles() {
-       TempFile::fileName('command');
-       TempFile::fileName('requires');
-       TempFile::fileName('state');
+        TempFile::fileName('command');
+        TempFile::fileName('requires');
+        TempFile::fileName('state');
     }
-
-
     private function initializeAutocompletion() {
         $phpList = get_defined_functions();
         $this->autocompleteList = array_merge($this->autocompleteList, $phpList['internal']);
@@ -78,296 +59,240 @@ class iphp
         $this->autocompleteList = array_merge($this->autocompleteList, get_declared_classes());
         $this->autocompleteList = array_merge($this->autocompleteList, get_declared_interfaces());
     }
-
     private function initializeTags() {
         $tagsFile = $this->options[self::OPT_TAGS_FILE];
-        if (file_exists($tagsFile))
-        {
+        if (file_exists($tagsFile)) {
             $tags = array();
             $tagLines = file($tagsFile);
-            foreach ($tagLines as $tag) {
+            foreach($tagLines as $tag) {
                 $matches = array();
-                if (preg_match('/^([A-z0-9][^\W]*)\W.*/', $tag, $matches))
-                {
+                if (preg_match('/^([A-z0-9][^\W]*)\W.*/', $tag, $matches)) {
                     $tags[] = $matches[1];
                 }
             }
             $this->autocompleteList = array_merge($this->autocompleteList, $tags);
         }
     }
-
     private function requireFiles() {
-        if ($this->options[self::OPT_REQUIRE])
-        {
-            if (!is_array($this->options[self::OPT_REQUIRE]))
-            {
+        if ($this->options[self::OPT_REQUIRE]) {
+            if (!is_array($this->options[self::OPT_REQUIRE])) {
                 $this->options[self::OPT_REQUIRE] = array($this->options[self::OPT_REQUIRE]);
             }
-						TempFile::writeToFile('requires', serialize($this->options[self::OPT_REQUIRE]));
+            TempFile::writeToFile('requires', serialize($this->options[self::OPT_REQUIRE]));
         }
     }
-
     /**
-		* Called when the main loop exits
-		* @return void
-		*/
-		public function cleanUp() {
-			TempFile::clear();
-		}
-		/**
-		* Returns the current prompt string
-		* @return string
-		*/
-    public function prompt()
-    {
+     * Called when the main loop exits
+     * @return void
+     */
+    public function cleanUp() {
+        TempFile::clear();
+    }
+    /**
+     * Returns the current prompt string
+     * @return string
+     */
+    public function prompt() {
         return $this->prompt;
     }
-		/**
-		* Returns the history file
-		* @return string
-		*/
-    public function historyFile()
-    {
+    /**
+     * Returns the history file
+     * @return string
+     */
+    public function historyFile() {
         return getenv('HOME') . '/.iphpHistory';
     }
-		/**
-		* Callback function for readline
-		* @param string $command
-		* @return void
-		*/
-    public function readlineCallback($command)
-    {
+    /**
+     * Callback function for readline
+     * @param string $command
+     * @return void
+     */
+    public function readlineCallback($command) {
         if ($command === NULL) exit;
         $this->lastCommand = $command;
     }
     /**
-		* Callback for auto completion
-		* @param string $str
-		* @return string
-		*/
-    public function readlineCompleter($str)
-    {
+     * Callback for auto completion
+     * @param string $str
+     * @return string
+     */
+    public function readlineCompleter($str) {
         return $this->autocompleteList;
     }
-    
-		/**
-		* Get function for specialCommands variable
-		* @return array
-		*/
-		public function specialCommands() {
-			return $this->specialCommands;
-		}
-		/**
-		* Processes special commands
-		* @param string $command
-		* @return void
-		*/
-		private function processSpecialCommands($command) {
-			switch($command) {
-				case 'exit':
-					$this->running = false;
-				break;
-				case 'reload!': 
-					TempFile::reset('state');
-					print("Cleaning Previous data\n");
-				break;
-				case 'help':
-					//Handle Help here
-					print("\n");
-				break;
-			}
-		}
-
-		/**
-		* This is the workhorse function that processes commands entered in the shell
-		* @param string $command
-		* @return void
-		*/
-    public function doCommand($command)
-    {
-				if(in_array($command, $this->specialCommands)) {
-					$this->processSpecialCommands($command);
-					return;
-				}
-        print "\n";
-        if (trim($command) == '')
-        {
+    /**
+     * Get function for specialCommands variable
+     * @return array
+     */
+    public function specialCommands() {
+        return $this->specialCommands;
+    }
+    /**
+     * Processes special commands
+     * @param string $command
+     * @return void
+     */
+    private function processSpecialCommands($command) {
+        switch ($command) {
+            case 'exit':
+                $this->running = false;
+            break;
+            case 'reload!':
+                TempFile::reset('state');
+                print ("Cleaning Previous data\n");
+            break;
+            case 'help':
+                //Handle Help here
+                print ("\n");
+            break;
+        }
+    }
+    /**
+     * This is the workhorse function that processes commands entered in the shell
+     * @param string $command
+     * @return void
+     */
+    public function doCommand($command) {
+        if (in_array($command, $this->specialCommands)) {
+            $this->processSpecialCommands($command);
             return;
         }
-
-        if(preg_match('/^(exit|die|quit|bye)(\(\))?;?$/', trim($command))){
+        print "\n";
+        if (trim($command) == '') {
+            return;
+        }
+        if (preg_match('/^(exit|die|quit|bye)(\(\))?;?$/', trim($command))) {
             die("\n");
         }
-
-        if (!empty($command) and function_exists('readline_add_history'))
-        {
+        if (!empty($command) and function_exists('readline_add_history')) {
             readline_add_history($command);
             readline_write_history($this->historyFile());
         }
-
-        $command = preg_replace('/^\//', '$_', $command);  // "/" as a command will just output the last result.
-
+        $command = preg_replace('/^\//', '$_', $command); // "/" as a command will just output the last result.
         $requires = unserialize(TempFile::readFromFile('requires'));
         if (!is_array($requires)) {
             $requires = array();
         }
-
-
-				$replacments = array('{$command}' => $command, '{$requires}' => var_export($requires, true), '{$requiresFile}' => TempFile::fileName('requires'), '{$stateFile}' => TempFile::fileName('state'));
+        $replacments = array('{$command}' => $command, '{$requires}' => var_export($requires, true), '{$requiresFile}' => TempFile::fileName('requires'), '{$stateFile}' => TempFile::fileName('state'));
         $parsedCommand = str_replace(array_keys($replacments), array_values($replacments), self::getTemplate('command'));
-				
-
         try {
             $_ = $this->lastResult;
-						TempFile::writeToFile('command', $parsedCommand);
+            TempFile::writeToFile('command', $parsedCommand);
             $result = NULL;
             $output = array();
-
-						$command_array = array($this->phpExecutable, TempFile::fileName('command'), '2>&1');
+            $command_array = array($this->phpExecutable, TempFile::fileName('command'), '2>&1');
             $lastLine = exec(implode(' ', $command_array), $output, $result);
-
-            if ($result != 0) throw( new Exception("Fatal error executing php: " . join("\n", $output)) );
-
+            if ($result != 0) throw (new Exception("Fatal error executing php: " . join("\n", $output)));
             // boostrap requires environment of command
             $requires = unserialize(TempFile::readFromFile('requires'));
-            foreach ($requires as $require) {
+            foreach($requires as $require) {
                 if ($require === TempFile::fileName('command')) continue;
-                require_once($require);
+                require_once ($require);
             }
-
-            
             $lastState = unserialize(TempFile::readFromFile('state'));
             $this->lastResult = $lastState['_'];
-
-            if ($lastState['__out'])
-            {
+            if ($lastState['__out']) {
                 print $lastState['__out'] . "\n";
-            }
-            else
-            {
-                if (is_object($this->lastResult) && !is_callable(array($this->lastResult, '__toString')))
-                {
+            } else {
+                if (is_object($this->lastResult) && !is_callable(array($this->lastResult, '__toString'))) {
                     print_r($this->lastResult) . "\n";
-                }
-                else
-                {
+                } else {
                     print $this->lastResult . "\n";
                 }
             }
-
             // after the eval, we might have new classes. Only update it if real readline is enabled
             if (!empty($this->autocompleteList)) $this->autocompleteList = array_merge($this->autocompleteList, get_declared_classes());
-        } catch (Exception $e) {
+        }
+        catch(Exception $e) {
             print "Uncaught exception with command:\n" . $e->getMessage() . "\n";
         }
     }
-		/**
-		* Sets up readline 
-		* @return mixed
-		*/
-    private function myReadline()
-    {
+    /**
+     * Sets up readline
+     * @return mixed
+     */
+    private function myReadline() {
         $this->lastCommand = NULL;
         readline_callback_handler_install($this->prompt, array($this, 'readlineCallback'));
         while ($this->lastCommand === NULL) {
             $w = NULL;
             $e = NULL;
-            $n = @stream_select($r = array(STDIN), $w, $e, NULL);       // @ to silence warning on ctl-c
-            if ($n === false) break;                                    // ctl-c or other signal
-            if (in_array(STDIN, $r))
-            {
+            $n = @stream_select($r = array(STDIN), $w, $e, NULL); // @ to silence warning on ctl-c
+            if ($n === false) break; // ctl-c or other signal
+            if (in_array(STDIN, $r)) {
                 readline_callback_read_char();
             }
         }
         readline_callback_handler_remove();
         return $this->lastCommand;
     }
-		/**
-		* Reads the input from the shell
-		* @return string
-		*/
-    public function readline()
-    {
-        if (function_exists('readline'))
-        {
+    /**
+     * Reads the input from the shell
+     * @return string
+     */
+    public function readline() {
+        if (function_exists('readline')) {
             $command = $this->myReadline();
-        }
-        else
-        {
-            $command = rtrim( fgets( STDIN ), "\n" );
+        } else {
+            $command = rtrim(fgets(STDIN), "\n");
             // catch ctl-d
-            if (strlen($command) == 0)
-            {
+            if (strlen($command) == 0) {
                 exit;
             }
         }
         return $command;
     }
-		/**
-		* Sends the signal for the shell to exit
-		* @return void
-		*/
-    public function stop()
-    {
+    /**
+     * Sends the signal for the shell to exit
+     * @return void
+     */
+    public function stop() {
         $this->running = false;
     }
-		/**
-		* This is the main application loop
-		* @return void
-		*/
-    public static function main($options = array())
-    {
+    /**
+     * This is the main application loop
+     * @return void
+     */
+    public static function main($options = array()) {
         $shell = new iphp($options);
-
         // install signal handlers if possible
         declare(ticks = 1);
-        if (function_exists('pcntl_signal'))
-        {
+        if (function_exists('pcntl_signal')) {
             pcntl_signal(SIGINT, array($shell, 'stop'));
         }
-
-				$special = implode(', ', $shell->specialCommands());
+        $special = implode(', ', $shell->specialCommands());
         print self::getTemplate('help');
-
         // readline history
-        if (function_exists('readline_read_history'))
-        {
-            readline_read_history($shell->historyFile());   // doesn't seem to work, even though readline_list_history() shows the read items!
+        if (function_exists('readline_read_history')) {
+            readline_read_history($shell->historyFile()); // doesn't seem to work, even though readline_list_history() shows the read items!
+            
         }
         // install tab-complete
-        if (function_exists('readline_completion_function'))
-        {
+        if (function_exists('readline_completion_function')) {
             readline_completion_function(array($shell, 'readlineCompleter'));
         }
-        while ($shell->running)
-        {
+        while ($shell->running) {
             $shell->doCommand($shell->readline());
         }
-				print("Good Bye\n");
-				$shell->cleanUp();
+        print ("Good Bye\n");
+        $shell->cleanUp();
     }
-
-		/**
-		* Finds the location of the PHP executable based off the PHP_BINDIR constant
-		* @return string
-		*/
-    public static function PHPExecutableLocation()
-    {
-        if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    /**
+     * Finds the location of the PHP executable based off the PHP_BINDIR constant
+     * @return string
+     */
+    public static function PHPExecutableLocation() {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             $phpExecutableName = 'php.exe';
         } else {
             $phpExecutableName = 'php';
         }
         return PHP_BINDIR . DIRECTORY_SEPARATOR . $phpExecutableName;
     }
-
-		/**
-		* Fetches a template files contents from the template directory
-		* @return string
-		*/
-		private static function getTemplate($file) {
-			return file_get_contents(implode(DIRECTORY_SEPARATOR, array(dirname(__FILE__) , 'templates/', $file . '.tmpl')));
-		}
-
-
+    /**
+     * Fetches a template files contents from the template directory
+     * @return string
+     */
+    private static function getTemplate($file) {
+        return file_get_contents(implode(DIRECTORY_SEPARATOR, array(dirname(__FILE__), 'templates/', $file . '.tmpl')));
+    }
 }

560337e4c8762298414bb4f9660761e8da09bd0d
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2009-12-12 02:21:45 -0500

    cleaned up source

--- lib/iphp_commands.php
+++ lib/iphp_commands.php
@@ -1,32 +1,21 @@
 <?php
-
-interface iphp_command
-{
+interface iphp_command {
     function run($shell, $args);
     function name();
 }
-
-class iphp_command_exit implements iphp_command
-{
-    function run($shell, $args)
-    {
+class iphp_command_exit implements iphp_command {
+    function run($shell, $args) {
         exit(0);
     }
-    function name()
-    {
+    function name() {
         return array('exit', 'die', 'bye', 'quit');
     }
 }
-
-class iphp_command_reload implements iphp_command
-{
-    function run($shell, $args)
-    {
+class iphp_command_reload implements iphp_command {
+    function run($shell, $args) {
         $shell->initialize($shell->options());
     }
-    function name()
-    {
+    function name() {
         return 'reload';
     }
 }
-
--- lib/temp_file.php
+++ lib/temp_file.php
@@ -1,60 +1,59 @@
 <?php
- 	/**
-	* This Class exposes TempFile handles so that they are mockable in test cases
-	*/
-	class TempFile {
-		/**
-		* File handle cache
-		*/
-		static $tempFiles = array();
-		/**
-		* Creates a temp file and caches its location
-		* @param string $name
-		* @return string - path of tempfile
-		*/		
-		public static function fileName($name) {
-			if(!isset(self::$tempFiles[$name])) {
-				self::$tempFiles[$name] = tempnam(sys_get_temp_dir(), "iphp.{$name}.");
-			}
-      return self::$tempFiles[$name];
+/**
+ * This Class exposes TempFile handles so that they are mockable in test cases
+ */
+class TempFile {
+    /**
+     * File handle cache
+     */
+    static $tempFiles = array();
+    /**
+     * Creates a temp file and caches its location
+     * @param string $name
+     * @return string - path of tempfile
+     */
+    public static function fileName($name) {
+        if (!isset(self::$tempFiles[$name])) {
+            self::$tempFiles[$name] = tempnam(sys_get_temp_dir(), "iphp.{$name}.");
+        }
+        return self::$tempFiles[$name];
     }
-		/**
-		* Writes supplied data to a temp file
-		* @param string $name - Shortname of file uses tmpFileName 
-		* @param string $data - Data to write to file
-		*/
-		public static function writeToFile($name, $data) {
-			file_put_contents(self::fileName($name), $data);
-		}
-		/**
-		* Reads from a temp file
-		* @param string $name - Shortname of file uses tmpFileName
-		* @return string
-		*/
-		public static function readFromFile($name) {
-			return file_get_contents(self::fileName($name));
-		}
-		/**
-		* Deletes all temp files and resets the cache
-		* Use this function to clean up the file system on exit
-		* @return void
-		*/
-		public static function clear() {
-			foreach(self::$tempFiles as $file) {
-				unlink($file);
-			}
-			self::$tempFiles = array();
-		}
-		/**
-		* Removes temp file and creates a new one
-		* @param string $name - Shortname of file uses tmpFileName
-		* @return void
-		*/
-		public static function reset($name) {
-			$path = self::fileName($name);
-			unlink($path);
-			unset(self::$tempFiles[$name]);
-			self::fileName($name);
-		}
-		
-	}
+    /**
+     * Writes supplied data to a temp file
+     * @param string $name - Shortname of file uses tmpFileName
+     * @param string $data - Data to write to file
+     */
+    public static function writeToFile($name, $data) {
+        file_put_contents(self::fileName($name), $data);
+    }
+    /**
+     * Reads from a temp file
+     * @param string $name - Shortname of file uses tmpFileName
+     * @return string
+     */
+    public static function readFromFile($name) {
+        return file_get_contents(self::fileName($name));
+    }
+    /**
+     * Deletes all temp files and resets the cache
+     * Use this function to clean up the file system on exit
+     * @return void
+     */
+    public static function clear() {
+        foreach(self::$tempFiles as $file) {
+            unlink($file);
+        }
+        self::$tempFiles = array();
+    }
+    /**
+     * Removes temp file and creates a new one
+     * @param string $name - Shortname of file uses tmpFileName
+     * @return void
+     */
+    public static function reset($name) {
+        $path = self::fileName($name);
+        unlink($path);
+        unset(self::$tempFiles[$name]);
+        self::fileName($name);
+    }
+}
